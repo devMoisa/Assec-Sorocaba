b333d151bb7e41921d999ebf5f20be0b597b6a8d
devmoises <moisesboas@gmail.com>
2024-09-14 21:48:42 -0300

    feat: agenda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import HomeDesktop from "@/desktop/pages/Home";
 import HomeMobile from "@/mobile/pages/Home";
+import "../global.css";
 
 export default function Home() {
   return (
--- src/desktop/modules/Agenda.tsx
+++ src/desktop/modules/Agenda.tsx
@@ -1,7 +1,27 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import { Container, Flex, Image, Text } from "@chakra-ui/react";
 
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 4,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 3,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 3,
+  },
+};
+
 const Agenda = () => {
-  // Array de objetos com as informações dos cards
   const cardData = [
     {
       imageSrc: "card01.png",
@@ -30,6 +50,22 @@ const Agenda = () => {
     {
       imageSrc: "card01.png",
       title: "Lorem Ipsum",
+      date: "20 de julho",
+      address: "Rua Bernardo Ferraz de Almeida, 172 Sala 03 - Jd. Faculdade",
+      description:
+        "A Filaharmônica Jovem de Sorocaba apresenta uma noite mágica com obras de Beethoven e Tchaikovsky. Um evento imperdível para os amantes da música clássica.",
+    },
+    {
+      imageSrc: "card02.png",
+      title: "Lorem Ipsum",
+      date: "21 de julho",
+      address: "Avenida Paulista, 1000 - São Paulo",
+      description:
+        "Um evento incrível com apresentações de grandes nomes da música popular brasileira.",
+    },
+    {
+      imageSrc: "card03.png",
+      title: "Lorem Ipsum",
       date: "22 de julho",
       address: "Praça da Liberdade, 50 - Belo Horizonte",
       description:
@@ -40,7 +76,9 @@ const Agenda = () => {
   const Card = ({ card }: any) => (
     <Flex
       flexDir={"column"}
-      width={"33%"}
+      width={"90%"}
+      maxW={"310px"}
+      height={"580px"}
       bg={"#ededed"}
       borderRadius={"4px"}
       paddingBottom={"30px"}
@@ -103,40 +141,42 @@ const Agenda = () => {
   );
 
   return (
-    <Flex mt={"8%"} mb={"8%"} flexDir={"column"}>
-      <Text
-        textAlign={"center"}
-        fontWeight={"normal"}
-        fontFamily={"Gotham"}
-        mb={"45px"}
-        fontSize={{
-          base: "20px",
-          sm: "25px",
-          lg: "30px",
-        }}
-        marginTop={{
-          base: "15%",
-          md: "0",
-        }}
-      >
-        AGENDA
-      </Text>
-      <Container
-        padding={0}
-        maxW={{
-          base: "8xl",
-          md: "6xl",
-          lg: "8xl",
-          xl: "8xl",
-        }}
-        display={"flex"}
-        gap={30}
-      >
-        {cardData.map((card, index) => (
-          <Card key={index} card={card} />
-        ))}
-      </Container>
-    </Flex>
+    <>
+      <Flex mt={"8%"} mb={"8%"} flexDir={"column"}>
+        <Text
+          textAlign={"center"}
+          fontWeight={"normal"}
+          fontFamily={"Gotham"}
+          mb={"45px"}
+          fontSize={{
+            base: "20px",
+            sm: "25px",
+            lg: "30px",
+          }}
+          marginTop={{
+            base: "15%",
+            md: "0",
+          }}
+        >
+          AGENDA
+        </Text>
+        <Container
+          padding={"25px"}
+          maxW={{
+            base: "8xl",
+            md: "6xl",
+            lg: "8xl",
+            xl: "8xl",
+          }}
+        >
+          <Carousel autoPlay arrows={true} responsive={responsive} infinite>
+            {cardData.map((card, index) => (
+              <Card key={index} card={card} />
+            ))}
+          </Carousel>
+        </Container>
+      </Flex>
+    </>
   );
 };
 
