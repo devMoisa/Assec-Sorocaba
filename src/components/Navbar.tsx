ee519bbc6b8ffb622809b27bab1d64cc116d00cb
Moisés Vilas Boas <moises.miranda@mblabs.com.br>
2024-09-30 03:44:27 -0300

    feat: navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,46 +1,73 @@
-import { Icon } from "@chakra-ui/react";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { FaFacebookSquare, FaInstagram, FaYoutube } from "react-icons/fa";
 import { IoMenuSharp } from "react-icons/io5";
-
+import { HiMinus, HiPlus } from "react-icons/hi2";
 export const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const [assecOpen, setAssecOpen] = useState(false);
+
   return (
     <>
       {/* Mobile */}
-      <nav
-        className="w-full h-24 bg-white flex items-center align-middle flex-row
-    lg:hidden
-      "
-      >
-        <div className="w-full flex justify-center items-center gap-5">
-          <Image
-            alt="asdasds"
-            src={"/logo_colored.png"}
-            height={60}
-            width={60}
-          />
-          <h1
-            className="
-                      #default
-                      text-3xl
-                      font-normal
-                      font-[var(--font-gotham), sans-serif]
-          "
-          >
-            ASSEC
-          </h1>
-        </div>
-        <button
-          className="bg-[#1366AC] h-full pl-5 pr-5
-        cursor-pointer
-        hover:opacity-90
-        hover:transition
-        "
+      <>
+        <nav className="w-full h-24 bg-white flex items-center align-middle flex-row lg:hidden relative">
+          <div className="w-full flex justify-center items-center gap-5">
+            <Image
+              alt="Assec Sorocaba"
+              src={"/logo_colored.png"}
+              height={60}
+              width={60}
+              className="ml-14"
+            />
+            <h1 className="text-3xl font-normal font-[var(--font-gotham), sans-serif]">
+              ASSEC
+            </h1>
+          </div>
+          <button className="bg-[#1366AC] h-full pl-5 pr-5 cursor-pointer hover:opacity-90 hover:transition">
+            <IoMenuSharp className="text-white text-5xl" />
+          </button>
+        </nav>
+        <div
+          id="navbarMobileOpen"
+          className="absolute top-25 right-0 w-full bg-[#F4F4F4] z-10"
         >
-          <IoMenuSharp className="text-white text-5xl" />
-        </button>
-      </nav>
+          <div className="pl-10 pr-10 pt-10">
+            <div className="flex justify-center">
+              <span className="text-[#1366AC]">PORTUGUÊS |</span>{" "}
+              <span className="text-gray-400 ml-2">ENGLISH</span>
+            </div>
+
+            <div
+              onClick={() => setAssecOpen(!assecOpen)}
+              className={`
+                ${assecOpen ? "bg-[#1366AC] " : "bg-[#ffff]"} 
+                ${assecOpen ? "text-white" : "text-[#1366AC]"} 
+                mt-10 w-full p-5 pl-7   flex flex-row justify-between items-center cursor-pointer hover:opacity-90 transition relative`}
+            >
+              <span className="font-light text-xl">A ASSEC</span>
+              <div
+                className={`absolute right-0 ${
+                  !assecOpen && "bg-[#1366AC]"
+                } h-full flex items-center p-2`}
+              >
+                {assecOpen && <HiMinus size={50} />}
+                {!assecOpen && <HiPlus size={50} color="#FFFF" />}
+              </div>
+            </div>
+
+            {assecOpen && (
+              <div className="w-full bg-[#e2e2e2] p-5 pl-7 pr-7 mb-5">
+                <a href="#">TIMELINE</a>
+                <hr className="mt-2 mb-2 text-3xl h-[1px] bg-[#cacaca]" />
+                <a href="#">MANIFESTO</a>
+              </div>
+            )}
+          </div>
+        </div>
+      </>
+
       {/* Desktop */}
       <nav className="absolute z-10 font-[var(--font-gotham), sans-serif] hidden lg:flex  w-full text-white pt-7 pb-10 uppercase">
         <div className="container flex flex-col">
